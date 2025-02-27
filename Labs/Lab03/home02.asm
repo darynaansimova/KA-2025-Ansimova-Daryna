5e87ad72dc102572b814e511f822d34af77d5ca6
darynaansimova <96005019+darynaansimova@users.noreply.github.com>
2025-02-27 12:42:16 +0200

    hw3 corrected

--- Labs/Lab03/home02.asm
+++ Labs/Lab03/home02.asm
@@ -1,9 +1,9 @@
 .model small
 .stack 100h
 .data
-    a dw 40
+    a dw 30
     b dw 400
-    c dw 1200
+    c dw 120
 .code
 main    proc
 
@@ -14,34 +14,42 @@ mov ax, a
 mov bx, b
 mov cx, c
 
-; if ((a < b) && (b > c))
-;   if (a != c)
+; if ((a < b && b > c)||(a < 10 && c >= b && a <= b)){
+;   if (a != 40){
 ;     a = b + c
-;   else
-;     b = a & c
-; else
+;   }
+;   b = a & c
+;}
+; else{
 ;   a = 0
 ;   b = 1
+;}
 
 comp1:
 cmp ax, bx ;(a < b)
-jnb comp2 ;if (a < b) = false then goto else
+jnb comp2 ;if (a < b) = false then goto comp2
 cmp bx, cx ;(b > c)
-jna comp2 ;if (b > c) = false then goto else
+jna comp2 ;if (b > c) = false then goto comp2
+comp2:
+cmp ax, 10d
+jnb comp1_else
+cmp cx, bx
+jb comp1_else
+cmp ax, bx
+ja comp1_else
 comp11:
-cmp ax, cx ;(a != c)
-jz comp12 ;if (a != c) = false then goto else
+cmp ax, 40d ;(a != 40)
+jz comp12 ;if (a != 40) = false then goto next
 add bx, cx 
 mov ax, bx ;a = b + c
 sub bx, cx
-jmp assign_vars
-comp12: ;else:
+comp12: ;next:
 mov dx, ax
 and dx, cx
 mov bx, dx ;b = a & c
 jmp assign_vars
 
-comp2:
+comp1_else:
 mov ax, 0 ;a = 0
 mov bx, 1 ;b = 1
 
